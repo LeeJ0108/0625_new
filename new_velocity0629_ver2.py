4009370c6ba96bca10f12a3c7407d60931f4ffc9
LeeJ0108 <pmlee0601@naver.com>
2024-06-29 21:19:24 +0900

    Add files via upload

---
+++ new_velocity0629_ver2.py
@@ -0,0 +1,375 @@
+import numpy as np
+from pyquaternion import Quaternion
+import rospy
+import numpy as np
+
+import time
+import matplotlib.pyplot as plt
+
+#!/usr/bin/env python
+# -- coding: utf-8 --
+
+# basic package
+from datetime import date
+import rospy
+import time
+from math import pi
+import math
+import numpy as np
+
+# message file
+from macaron_6.msg import erp_read
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Point
+import matplotlib.pyplot as plt
+from ublox_msgs.msg import NavPVT
+from math import atan2 ,pi, sin, cos
+from sensor_msgs.msg import NavSatFix
+
+
+from tf.transformations import euler_from_quaternion
+
+from pyproj import Proj, Transformer, CRS
+
+import time
+import csv
+
+proj_UTMK = CRS(init='epsg:5179')
+proj_WGS84 = CRS(init='epsg:4326')
+
+
+class Position:
+    def __init__(self):
+        self.sub_imu = rospy.Subscriber('/imu/data', Imu, self.imu_callback, queue_size=1)
+        self.sub_gps = rospy.Subscriber("ublox_gps/fix", NavSatFix, self.gps_callback,queue_size=1)
+        self.erp_sub = rospy.Subscriber('erp_read', erp_read, self.erp_callback, queue_size=1)
+
+        self.GPS_INPUT_TIME = time.time()
+        self.GPS_LAST_TIME = time.time()
+        self.pos = [0,0]
+        self.last_pos = [0,0]
+        self.GPS_init_Count = 0
+
+        self.velocity = 0
+        
+        self.IMU_init_Count = 0
+        self.IMU_last_accel_x = 0
+        self.linear_x = 0
+        self.linear_y = 0
+        self.linear_z= 0
+        self.IMU_last_Time  = time.time()
+        self.IMU_input_Time = time.time()
+        self.IMU_vel_total= 0
+        self.last_velocity = 0
+        self.Time_interval = 0
+
+        self.timer = 0.1
+        self.count = 0
+        self.yaw  = 0
+        self.pitch = 0
+        self.linear_x2  = 0
+        self.linear_y2= 0
+        self.linear_z2 = 0
+        self.IMU_vel_x =0
+
+        self.erp_init_Count= 0
+        self.last_enc = 0
+        self.enc = 0 
+        self.erp_velocity2  = 0
+        self.erp_INPUT_TIME = time.time()
+        self.erp_LAST_TIME = time.time()
+        self.dt = 0
+
+
+    def erp_callback(self, data):
+        self.steer = (data.read_steer)/71
+        self.enc = data.read_ENC
+        self.erp_INPUT_TIME = time.time()
+
+    def erp_Velocity(self):
+        if self.erp_init_Count == 0:
+           
+            self.erp_LAST_TIME = self.erp_INPUT_TIME
+            self.last_enc = self.enc  
+            self.erp_count = 0
+            self.erp_init_Count = 1    
+      
+        elif self.erp_init_Count == 1:    
+            if self.last_enc !=  self.enc:
+                self.dt = abs(self.erp_INPUT_TIME-self.erp_LAST_TIME)
+                self.erp_velocity2 = 0.5*abs(self.enc - self.last_enc)*(2*np.pi/100)/2/self.dt * 3.6  # 바퀴 지름 50cm
+                if self.erp_velocity2 >50:
+                    self.erp_velocity2 = 0
+                self.erp_LAST_TIME = self.erp_INPUT_TIME
+                self.last_enc = self.enc 
+                self.erp_count = 0
+
+
+        return self.erp_velocity2 
+    
+    def gps_callback(self, gps_msg):
+        
+        lon = gps_msg.longitude
+        lat = gps_msg.latitude
+        transformer = Transformer.from_crs(proj_WGS84, proj_UTMK)
+        x,y = transformer.transform(lon,lat)
+        self.pos = [x, y]
+        self.GPS_INPUT_TIME = time.time()
+        if self.count < 5:
+            self.count += 1
+        
+    def GPS_VELOCITY(self):
+        if self.GPS_init_Count == 0:
+           
+            self.GPS_LAST_TIME = self.GPS_INPUT_TIME
+            self.last_pos = self.pos  
+            if self.count > 4:
+                # print(self.count)
+                self.GPS_init_Count = 1
+              
+        elif self.GPS_init_Count == 1:    
+            self.Time_interval = abs(self.GPS_INPUT_TIME- self.GPS_LAST_TIME)
+            if self.Time_interval > self.timer:
+                distance = np.hypot(self.pos[0] - self.last_pos[0],self.pos[1] - self.last_pos[1])
+                self.velocity = distance/self.Time_interval *3.6
+                
+
+                self.last_pos = self.pos
+                self.GPS_LAST_TIME = self.GPS_INPUT_TIME
+                self.last_velocity = self.velocity
+
+        return self.velocity
+
+    def GPS_POS(self):
+        return self.pos       
+        
+    def imu_callback(self, imu): #임시로 마이너스
+        self.linear_x = -imu.linear_acceleration.x
+        self.linear_y = -imu.linear_acceleration.y 
+        self.linear_z = imu.linear_acceleration.z 
+        self.angular_z = imu.angular_velocity.z 
+        self.magnetic_field_x = imu.orientation.x
+        self.magnetic_field_y = imu.orientation.y
+        self.magnetic_field_z = imu.orientation.z
+        self.IMU_input_Time = time.time()
+        
+    
+        quaternion = (imu.orientation.x, imu.orientation.y, imu.orientation.z, imu.orientation.w)
+        roll, self.pitch, self.yaw = euler_from_quaternion(quaternion)
+        if self.yaw  < 0:
+            self.yaw  += 2*np.pi
+ 
+
+        measurement =[[self.linear_x],[self.linear_y],[self.linear_z]]
+        angular = self.pitch
+        angular_array = [[np.cos(angular),0,np.sin(angular)],[0,1,0],[-np.sin(angular),0,np.cos(angular)]]
+        
+        gravity_vector = np.array([[0],[0],[9.80665]])
+        result = measurement - np.dot(np.transpose(angular_array) , gravity_vector)
+
+        a = 8
+        self.linear_x2 = int(result[0][0] * 10**a) / 10**a
+        self.linear_y2 = int(result[1][0] * 10**a) / 10**a
+        self.linear_z2 = int(result[2][0] * 10**a) / 10**a
+
+
+    def IMU_HEADING(self):
+        return self.yaw        
+            
+    def IMU_velocity_calc(self):
+        if self.IMU_init_Count == 0:
+            
+            self.IMU_last_accel_x = self.linear_x2
+            self.IMU_last_accel_y = self.linear_y2
+            self.IMU_last_Time  = time.time()
+            self.IMU_init_Count = 1
+        
+            
+        elif self.IMU_init_Count == 1:
+            IMU_time_intervel = self.IMU_input_Time -self.IMU_last_Time 
+            if IMU_time_intervel >= 0.001:  
+                self.IMU_vel_x = (self.linear_x2+self.IMU_last_accel_x)*IMU_time_intervel/2 
+                self.IMU_vel_y = (self.linear_y2+self.IMU_last_accel_y) * IMU_time_intervel/2 
+
+                i = 1
+                if self.IMU_vel_x > 0:
+                    i = 1
+                elif self.IMU_vel_x == 0:
+                    i = 0
+                else:
+                    i = -1    
+                # self.IMU_vel_total += i*np.sqrt(self.IMU_vel_x**2+self.IMU_vel_y**2) * 3.6
+                self.IMU_vel_total += self.IMU_vel_x * 3.6
+                
+                if self.IMU_vel_total == 0:
+                    self.vel_error = 0
+                if self.IMU_vel_total<0:
+                    self.IMU_vel_total = 0  
+                    self.vel_error = 0
+
+                self.IMU_last_Time = self.IMU_input_Time
+                self.IMU_last_accel_x  = self.linear_x2
+                self.IMU_last_accel_y  = self.linear_y2
+             
+        return self.IMU_vel_total
+
+
+class KalmanFilter:
+    def __init__(self, process_variance, measurement_variance, estimation_error, initial_value):
+        self.process_variance = process_variance
+        self.measurement_variance = measurement_variance
+        self.estimation_error = estimation_error
+        self.value = initial_value
+        self.kalman_gain = 0
+
+    def update(self, measurement):
+        # Prediction update
+        self.estimation_error += self.process_variance
+
+        # Measurement update
+        self.kalman_gain = self.estimation_error / (self.estimation_error + self.measurement_variance)
+        self.value += self.kalman_gain * (measurement - self.value)
+        self.estimation_error *= (1 - self.kalman_gain)
+
+        return self.value
+    
+
+class KalmanFilter_IMU:
+    def __init__(self, process_variance, measurement_variance, estimation_error, initial_value):
+        self.process_variance = process_variance
+        self.measurement_variance = measurement_variance
+        self.estimation_error = estimation_error
+        self.value = initial_value
+        self.kalman_gain = 0
+
+    def update(self, measurement):
+        # Prediction update
+        self.estimation_error += self.process_variance
+
+        # Measurement update
+        self.kalman_gain = self.estimation_error / (self.estimation_error + self.measurement_variance)
+        self.value += self.kalman_gain * (measurement - self.value)
+        self.estimation_error *= (1 - self.kalman_gain)
+
+        return self.value    
+
+def main():
+    rospy.init_node('mapping', anonymous=True)
+    p = Position()
+    rate = rospy.Rate(10)
+
+    init_pos = [0,0]
+    while init_pos == [0,0]:
+        init_pos = p.GPS_POS()
+
+    GPS_vel= 0
+    IMU_vel = 0
+    ENC_vel = 0
+
+    count_plot = []
+    GPS_plot = []
+    IMU_plot = []
+    ENC_plot = []
+
+    count = 0
+
+    # 칼만 필터 초기화
+    process_variance = 1e-5  # 프로세스 노이즈 공분산
+    measurement_variance = 0.00095  # 측정 노이즈 공분산
+    estimation_error = 1.0  # 초기 추정 오차
+    initial_value = 0 # 초기 값
+
+    kf = KalmanFilter(process_variance, measurement_variance, estimation_error, initial_value)
+
+    process_variance_1 = 1e-5  # 프로세스 노이즈 공분산
+    measurement_variance_1 = 8.645e-5   # 측정 노이즈 공분산
+    estimation_error_1 = 1.0  # 초기 추정 오차
+    initial_value_1 = 0 # 초기 값
+    kf_1 = KalmanFilter_IMU(process_variance_1, measurement_variance_1, estimation_error_1, initial_value_1)
+
+    # 필터를 적용한 데이터
+    filtered_velocities = []
+    filtered_velocities_1 = []
+
+    last_timer = time.time()
+    last_ENC_VEL = 0
+
+    while not rospy.is_shutdown():  
+        GPS_vel= round(p.GPS_VELOCITY(),3)
+        IMU_vel = round(p.IMU_velocity_calc(),3)
+        ENC_vel = round(p.erp_Velocity(),3)
+        count += 1
+
+        print(GPS_vel,IMU_vel,ENC_vel)
+        
+        filtered_value = kf.update(ENC_vel)
+        filtered_velocities.append(filtered_value)
+
+        # error = 0.1
+        # if ENC_vel >= last_ENC_VEL+round(p.IMU_vel_x*3.6,1)  - error and ENC_vel <= last_ENC_VEL+round(p.IMU_vel_x*3.6,1)  + error:
+        #     ENC_vel = ENC_vel
+        #     print("-----------",p.IMU_vel_x)
+
+        # else:
+        #     ENC_vel = last_ENC_VEL+p.IMU_vel_x*3.6
+        #     print("-----------------------------",p.IMU_vel_x)
+        
+        # last_ENC_VEL = ENC_vel
+
+        count_plot.append(count)
+        GPS_plot.append(GPS_vel)
+        IMU_plot.append(IMU_vel)
+        ENC_plot.append(ENC_vel)
+
+        if ENC_vel <1:
+            filtered_value = 0
+
+        if p.linear_x2 >= 0:
+            time_interval = time.time() - last_timer
+            if time_interval > 0.1:
+                p.IMU_vel_total = filtered_value
+                last_timer = time.time()
+
+
+        # filtered_value_1 = kf_1.update(p.IMU_vel_x)
+        filtered_value_1 = kf_1.update(IMU_vel)
+        filtered_velocities_1.append(filtered_value_1)
+
+        if count > 300:
+            break
+        rate.sleep()
+        
+ 
+    start = 5    
+    plt.figure(1)
+    plt.plot(count_plot[start:], GPS_plot[start:], label='GPS', color='red')
+    plt.plot(count_plot[start:], IMU_plot[start:], label='IMU', color='blue')
+    plt.plot(count_plot[start:], ENC_plot[start:], label='ENC', color='green')
+    plt.plot(count_plot[start:], filtered_velocities[start:], label='ENC_filter', color='black')
+    plt.xlabel('count')
+    plt.ylabel('velocity')
+    plt.title('Velocity')
+
+    plt.figure(2)
+    plt.plot(count_plot[start:], filtered_velocities_1[start:], label='IMU', color='blue')
+    plt.xlabel('count')
+    plt.ylabel('accel')
+    plt.title('accel')
+
+    plt.show()
+
+        # 측정 노이즈 공분산 행렬 추정
+    ENC_plot = np.array(ENC_plot)
+    GPS_plot = np.array(GPS_plot)
+
+    measurement_errors = ENC_plot - GPS_plot
+    R = np.cov(measurement_errors.T)
+
+
+    print("추정된 측정 노이즈 공분산 행렬 (R):")
+    print(R) 
+
+
+
+if __name__ == "__main__":
+    main()                          
